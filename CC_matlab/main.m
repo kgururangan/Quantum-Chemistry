acd58c06ca29ba73b4c56d818720c8814730d4fd
Karthik Gururangan <40921315+kgururangan@users.noreply.github.com>
2020-06-23 13:50:20 -0500

    Add files via upload

--- CC_matlab/main.m
+++ CC_matlab/main.m
@@ -12,19 +12,19 @@ addpath(genpath('/Users/karthik/Dropbox/Davidson_methods/davidson_cheb_test/Bchd
 addpath(genpath('/Users/karthik/Desktop/CC_matlab_tests/h2o-pvdz/'));
 addpath(genpath('/Users/karthik/Desktop/CC_matlab_tests/h2o-631g/'));
 addpath(genpath('/Users/karthik/Desktop/CC_matlab_tests/h2-pvdz/'));
+addpath(genpath('/Users/karthik/Desktop/CC_matlab_tests/rectangle-d2h-pvdz/'));
 
-
+%%
 %load h2o-pvdz-integrals.mat
 %load h2-pvdz-integrals.mat
-%load h2o-631g-stretched-integrals
-load h2o-631g-eom-ccpy
+load h2o-631g-stretched-integrals
+%load h2o-631g-eom-ccpy
+%load rectangle-pvdz-d2h
+
+nfzc = 0; nfzv = 0; nact_h = 200; nact_p = 200;
 
 sys_ucc = build_system_ucc(e1int,e2int,Nocc_a,Nocc_b);
-sys_cc = build_system_cc(e1int,e2int,Nocc_a+Nocc_b);
-
-
-% load h2o-pvdz-ccpy.mat
-%sys_cc = build_system(VM,FM,occ,unocc,2);
+sys_cc = build_system_cc(e1int,e2int,Nelec,nfzc,nfzv,nact_h,nact_p);
 
 %% UCCSD
 
@@ -34,7 +34,7 @@ sys_cc = build_system_cc(e1int,e2int,Nocc_a+Nocc_b);
 
 ccopts.diis_size = 3;
 ccopts.maxit = 100;
-ccopts.tol = 1e-8;
+ccopts.tol = 1e-9;
 [t1a,t1b,t2a,t2b,t2c,Ecorr_ucc] = uccsd(sys_ucc,ccopts);
 [T1] = convert_spinint_to_spinorb({t1a,t1b},sys_ucc);
 [T2] = convert_spinint_to_spinorb({t2a,t2b,t2c},sys_ucc);
@@ -51,14 +51,14 @@ E1A1B_exact =   5.956425520525440E-004;
 E1B1B_exact =   -7.721105255510347E-005;
 Ecorr_exact = E1A_exact + E1B_exact + E2A_exact + E2B_exact + E2C_exact + E1A1A_exact + E1B1B_exact + E1A1B_exact;
 
-E1A = einsum(sys_ucc.fa_ov,t1a,'me,em->');
-E1B = einsum(sys_ucc.fb_ov,t1b,'me,em->');
-E2A = 0.25*einsum(sys_ucc.vA_oovv,t2a,'mnef,efmn->');
-E2C = 0.25*einsum(sys_ucc.vC_oovv,t2c,'mnef,efmn->');
-E2B = einsum(sys_ucc.vB_oovv,t2b,'mnef,efmn->');
-E1A1A = 0.5*einsum(einsum(sys_ucc.vA_oovv,t1a,'mnef,fn->me'),t1a,'me,em->');
-E1B1B = 0.5*einsum(einsum(sys_ucc.vC_oovv,t1b,'mnef,fn->me'),t1b,'me,em->');
-E1A1B = einsum(einsum(sys_ucc.vB_oovv,t1a,'mnef,em->nf'),t1b,'nf,fn->');
+E1A = einsum_kg(sys_ucc.fa_ov,t1a,'me,em->');
+E1B = einsum_kg(sys_ucc.fb_ov,t1b,'me,em->');
+E2A = 0.25*einsum_kg(sys_ucc.vA_oovv,t2a,'mnef,efmn->');
+E2C = 0.25*einsum_kg(sys_ucc.vC_oovv,t2c,'mnef,efmn->');
+E2B = einsum_kg(sys_ucc.vB_oovv,t2b,'mnef,efmn->');
+E1A1A = 0.5*einsum_kg(einsum_kg(sys_ucc.vA_oovv,t1a,'mnef,fn->me'),t1a,'me,em->');
+E1B1B = 0.5*einsum_kg(einsum_kg(sys_ucc.vC_oovv,t1b,'mnef,fn->me'),t1b,'me,em->');
+E1A1B = einsum_kg(einsum_kg(sys_ucc.vB_oovv,t1a,'mnef,em->nf'),t1b,'nf,fn->');
 
 Ecorr = E1A + E1B + E2A + E2B + E2C + E1A1A + E1B1B + E1A1B;
 
@@ -73,40 +73,43 @@ fprintf('E1A1B error = %4.12f\n',E1A1B_exact - E1A1B)
 fprintf('Ecorr error = %4.12f\n',Ecorr_exact - Ecorr)
 
         
-%% CCSD
+
+%% Ground-state CC
 
 ccopts.diis_size = 3;
 ccopts.maxit = 100;
 ccopts.tol = 1e-9;
 
-[t1,t2,Ecorr_cc] = ccsd(sys_cc,ccopts);
+[t1,t2,Ecorr_ccsd] = ccsd(sys_cc,ccopts);
+
+% CCSDT errors on order of 10^-5... be VERY careful checking all antisymmetrizers with einsum!
+%[t1,t2,t3,Ecorr_ccsdt] = ccsdt(sys_cc,ccopts);
+[t1,t2,t3,Ecorr_ccsdt3] = ccsdt3(sys_cc,ccopts);
 
 
 %% Build HBar
 
 [HBar] = build_HBar(t1,t2,sys_cc); 
-[D, Dia, Dijab] = HBar_CCSD_diagonal(HBar, t1, t2, sys_cc);
-[Nocc,Nunocc] = size(HBar{1}{1,2}); Nov = Nocc*Nunocc; Hbar_dim = Nov + Nov^2;
 
-%% Left-CCSD ground state
+%% Left-CCSD 
 
 ccopts.diis_size = 5;
 ccopts.maxit = 200;
 ccopts.tol = 1e-9;
 
-[lambda1,lambda2,lcc_resid] = lccsd(0.0,[],t1,t2,HBar,sys_cc,ccopts);
+[lambda1,lambda2,lcc_resid] = lccsd(t1,t2,HBar,sys_cc,ccopts);
 
 
 %% EOM-CCSD
 
-eomopts.nroot = 15;
+eomopts.nroot = 3;
 eomopts.maxit = 1000;
-eomopts.tol = 1e-7;
+eomopts.tol = 1e-6;
 eomopts.nvec_per_root = 1;
-eomopts.max_nvec_per_root = 10;
+eomopts.max_nvec_per_root = 5;
 eomopts.flag_verbose = 1;
 eomopts.init_guess = 'cis';
-eomopts.thresh_vec = 1e-7; % normally set to the convergence threshold of eom
+eomopts.thresh_vec = 10*eomopts.tol;
 
 % it's possible there's an error in EOMCCSD sigma function. R vector for
 % first singlet state of H2 are close to Jun's but not exactly identical...
@@ -132,9 +135,9 @@ eomopts.thresh_vec = 1e-7; % normally set to the convergence threshold of eom
 
 ccopts.maxit = 100;
 ccopts.diis_size = 3;
-ccopts.tol = 1e-6; % can only converge Left-EOM to the same tolerance as we converged right EOM
+ccopts.tol = 10*eomopts.tol; % can only converge Left-EOM to the same tolerance as we converged right EOM
 
-[Lvec,eomlcc_resid] = lefteomccsd(omega,Rvec,HBar,t1,t2,sys_cc,ccopts);
+[Lvec,omega_lcc,eom_lcc_resid] = lefteomccsd(omega,Rvec,HBar,t1,t2,sys_cc,ccopts);
 
 
 
