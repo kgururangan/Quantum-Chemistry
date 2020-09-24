b7bf78bb677129f171d1a56436c4d3a9f3718ba0
Karthik Gururangan <kgururangan@gmail.com>
2020-09-23 22:08:10 -0700

    Add files via upload

--- CC_matlab/scf/uhf.m
+++ CC_matlab/scf/uhf.m
@@ -1,6 +1,5 @@
-function [Escf, C, P, eps_mo, Fock] = rhf(sys,opts,P_guess)
+function [Escf, C, P, eps_mo, Fock_A] = uhf(sys,opts,P_guess)
 
-    Nelec = sys.Nelec;
     Smat = sys.overlap;
     Zmat = sys.e1int;
     VVmat = sys.e2int;
@@ -11,10 +10,11 @@ function [Escf, C, P, eps_mo, Fock] = rhf(sys,opts,P_guess)
     maxit = opts.maxit;
     tol = opts.tol;
     
-    fprintf('\n==================================++Entering RHF Routine++=================================\n')
+    fprintf('\n==================================++Entering UHF Routine++=================================\n')
     
     Norb = size(Smat,1);    
-    Nocc = Nelec/2;
+    Nocc_a = sys.Nocc_a;
+    Nocc_b = sys.Nocc_b;
 
     tic_Start = tic;
     
@@ -23,25 +23,27 @@ function [Escf, C, P, eps_mo, Fock] = rhf(sys,opts,P_guess)
     
     % Initial guess at P 
     if nargin < 3
-        P = zeros(Norb);
+        Pa = zeros(Norb);
+        Pb = zeros(Norb) + 1e-3*rand(Norb);
     else
-        P = P_guess;
+        Pa = P_guess{1};
+        Pb = P_guess{2};
     end
     
 %     % Calculate Fock matrix with guess
-    G = einsum_kg(VVmat,P,'prqs,rs->pq') - ...
-        0.5*einsum_kg(VVmat,P,'prsq,rs->pq');    
-    Fock = Zmat + G;% + level_shift*(Smat-Smat.*P.*Smat);
+    Ga = einsum_kg(VVmat,Pa,'prqs,rs->pq') - ...
+        0.5*einsum_kg(VVmat,Pa,'prsq,rs->pq');    
+    Fock_A = Zmat + Ga;% + level_shift*(Smat-Smat.*P.*Smat);
     
     % Diagonalize Fock and get MO vectors and energies
-    [Cp,eps_mo] = eig(X'*Fock*X); eps_mo = diag(eps_mo);
+    [Cp,eps_mo] = eig(X'*Fock_A*X); eps_mo = diag(eps_mo);
     [~,idx] = sort(eps_mo,'ascend'); eps_mo = eps_mo(idx);   
     C = X*Cp(:,idx);
     
     % Build density matrix
     P = 2*einsum_kg(C(:,1:Nocc),C(:,1:Nocc),'pi,qi->pq');
 
-    Escf = 0.5*einsum_kg(P,Zmat+Fock,'pq,qp->') + Vnuc;
+    Escf = 0.5*einsum_kg(P,Zmat+Fock_A,'pq,qp->') + Vnuc;
 
     % DIIS containers
     F_list = zeros(Norb^2,diis_size); F_resid_list = zeros(Norb^2,diis_size);
@@ -53,35 +55,35 @@ function [Escf, C, P, eps_mo, Fock] = rhf(sys,opts,P_guess)
         tic
         
         % Calculate Fock matrix 
-        G = einsum_kg(VVmat,P,'prqs,rs->pq') - ...
+        Ga = einsum_kg(VVmat,P,'prqs,rs->pq') - ...
             0.5*einsum_kg(VVmat,P,'prsq,rs->pq');
 
-        Fock = Zmat + G + level_shift*(Smat-Smat*P*Smat);
+        Fock_A = Zmat + Ga + level_shift*(Smat-Smat*P*Smat);
 
         % Build DIIS Residual
-        diis_r = X*(Fock*P*Smat - Smat*P*Fock)*X;
+        diis_r = X*(Fock_A*P*Smat - Smat*P*Fock_A)*X;
         scf_resid = sqrt(mean(diis_r(:).^2));
       
         % append trial and residual vectors to lists
-        F_list(:,mod(it_micro,diis_size)+1) = Fock(:);
+        F_list(:,mod(it_micro,diis_size)+1) = Fock_A(:);
         F_resid_list(:,mod(it_micro,diis_size)+1) = diis_r(:);
          
         % diis extrapolate
         if mod(it_micro,diis_size) == 0 && it_micro > 0
            it_macro = it_macro + 1;
            fprintf('\nDIIS Cycle - %d',it_macro)
-           Fock = diis_xtrap(F_list,F_resid_list);
-           Fock = reshape(Fock,[Norb,Norb]);
+           Fock_A = diis_xtrap(F_list,F_resid_list);
+           Fock_A = reshape(Fock_A,[Norb,Norb]);
         end    
         
         % Diagonalize Fock and get MO vectors and energies
-        [Cp,eps_mo] = eig(X'*Fock*X); eps_mo = diag(eps_mo);
+        [Cp,eps_mo] = eig(X'*Fock_A*X); eps_mo = diag(eps_mo);
         [~,idx] = sort(eps_mo,'ascend'); eps_mo = eps_mo(idx);
         C = X*Cp(:,idx);
 
         % Compute HF energy and density matrix 
         P = 2*einsum_kg(C(:,1:Nocc),C(:,1:Nocc),'pi,qi->pq');
-        Escf = 0.5*einsum_kg(P,Zmat+Fock,'qp,pq->') + Vnuc;
+        Escf = 0.5*einsum_kg(P,Zmat+Fock_A,'qp,pq->') + Vnuc;
 
         fprintf('\n    Iter-%d     Residuum = %4.12f      Escf = %4.12f      Elapsed Time = %4.2f s',it_micro,scf_resid,Escf,toc);
         
